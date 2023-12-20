4dc956fc975d8edd928b63dcd783ac116056f3ea
Tarsis <br.tmvdl@gmail.com>
2023-12-20 06:44:50 -0300

    Updating app files

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+import fs from 'fs'
+import path from 'path'
 
 export class Logger {
+  path = null
   name = null
 
   constructor(name = 'Logger') {
@@ -9,5 +12,20 @@ export class Logger {
   log(key, ...values) {
     console.log(this.name, key, ...values)
     return this
+  }
+
+  file(key, value = '') {
+    const filename = path.resolve(this.getPath(), key)
+    fs.writeFileSync(filename, value)
+    return this
+  }
+
+  setPath(path = '.') {
+    this.path = path
+    return this
+  }
+
+  getPath() {
+    return path.resolve('.', this.path)
   }
 }
